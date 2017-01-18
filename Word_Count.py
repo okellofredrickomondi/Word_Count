58a8fadcdaec202d2af2e650b2be7a4e7058774e
Fredrick Omondi Okello <okellofredrickomondi@gmail.com>
2017-01-18 16:55:51 +0300

    Word_count_labs
    word-count_solution

---
+++ Word_Count.py
@@ -0,0 +1,7 @@
+Python 2.7.12 (v2.7.12:d33e0cf91556, Jun 27 2016, 15:19:22) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> str = "olly olly in come free";
+print str.split( )
+#print str.split(' ', 1 ) << not necessary
+for i in str:
+   print (i)
